cf0bb351cb81b4bd6f3b095eb3f798e98ce29238
Robert162001 <badalutarobert@gmail.com>
2023-01-03 13:42:38 +0200

    Third Commit

--- Lab7.py
+++ Lab7.py
@@ -8,9 +8,15 @@ response = requests.get(requests_url)
 
 if response.status_code == 200:
     data = response.json()
-    vreme=data['weather'][0]['description']
+    vreme = data['weather'][0]['description']
     temperature = round(data["main"]["temp"]-213.15 , 2)
+    vantul = data['wind']['speed']
+    presiunea = data['main']['pressure']
     print("Vremea: ", vreme)
     print("Temperatura: ", temperature)
+    print("Vantul: ", vantul)
+    print("Presiunea: ", presiunea)
+    f = open("file.txt", "w")
+    f.write("presiunea: "+str(presiunea)+'/n'+("vantu: "))
 else:
     print("Am o eroare")
